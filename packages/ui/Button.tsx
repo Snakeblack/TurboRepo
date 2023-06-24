4b2a525d644158553788ba8ed239d0471e66c385
sn4ke-desktop <michael.retamozo@gmail.com>
2023-06-24 20:00:25 +0200

    add and configuration -> storybook turborepo

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,7 +1,27 @@
-"use client";
+'use client'
 
-import * as React from "react";
+interface ButtonProps {
+  primary?: boolean
+  size?: 'small' | 'medium' | 'large'
+  label?: string,
+  backgroundColor?: string
+}
 
-export const Button = () => {
-  return <button onClick={() => alert("boop")}>Boop</button>;
-};
+export const Button = ({
+  primary = false,
+  size = 'small',
+  label = 'Boop',
+}: ButtonProps) => {
+  return (
+    <button
+      style={{
+        backgroundColor: primary ? 'blue' : 'red',
+        fontSize:
+          size === 'small' ? '1rem' : size === 'medium' ? '2rem' : '3rem'
+      }}
+      onClick={() => alert('boop')}
+    >
+      {label}
+    </button>
+  )
+}
